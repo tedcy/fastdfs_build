36a96483281322fc0bcd04da3ef11def1eee6109
tedcy <yue.cheng.ted@gmail.com>
2014-10-24 15:32:52 +0800

    add mount

---
+++ mount.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+mkdir -pv /data/fastdfs/data0
+mkdir -pv /data/fastdfs/data1
+mkdir -pv /data/fastdfs/data2
+mkdir -pv /data/fastdfs/data3
+mkdir -pv /data/fastdfs/data4
+mkdir -pv /data/fastdfs/data5
+mkdir -pv /data/fastdfs/data6
+mkdir -pv /data/fastdfs/data7
+mkdir -pv /data/fastdfs/data8
+mkdir -pv /data/fastdfs/data9
+mount /dev/sdb /data/fastdfs/data0
+mount /dev/sdc /data/fastdfs/data1
+mount /dev/sdd /data/fastdfs/data2
+mount /dev/sde /data/fastdfs/data3
+mount /dev/sdf /data/fastdfs/data4
+mount /dev/sdh /data/fastdfs/data5
+mount /dev/sdi /data/fastdfs/data6
+mount /dev/sdj /data/fastdfs/data7
+mount /dev/sdk /data/fastdfs/data8
+mount /dev/sdl /data/fastdfs/data9
